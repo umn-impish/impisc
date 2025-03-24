b2d8678c28ef067e3fa589d70efd24c74fa07538
William Setterberg <sette095@umn.edu>
2025-03-24 17:24:18 -0500

    add some bits for command acks on the ground

--- ground/support.py
+++ ground/support.py
@@ -1,6 +1,12 @@
+'''
+Classes and such which support the ground system
+'''
 import socket
+import queue
+
 from impisc.network import grips_given
 from impisc.network import packets
+
 
 class PacketDiscriminator:
     '''
@@ -38,3 +44,36 @@ class PacketDiscriminator:
 
         for ep in endpoints:
             self.resender.sendto(packet, ep)
+
+
+class DecodedCommandAck:
+    '''Decode a command "ack" packet into something a little prettier'''
+    def __init__(self, packet: grips_given.CommandAcknowledgement):
+        self.good = (packet.error_type == packet.NO_ERROR)
+        self.issue = packet.HUMAN_READABLE_FAILURES[
+            packet.error_type
+        ]
+        self.command_number = packet.counter
+        self.data = packet.error_data
+        self.type = packets.all_commands[packet.cmd_type]
+
+
+class CommandAckQueue:
+    '''
+    A queue that receives packets and holds onto acks.
+    The acks get formatted as objects instead of raw GRIPS packets,
+    which makes them easier to touch elsewhere in the program.
+    '''
+    def __init__(
+        self,
+        data_stream: socket.socket,
+        history_length: int=10,
+    ):
+        self.stream = data_stream
+        self.queue = queue.Queue(maxsize=history_length)
+
+    def accept_new(self):
+        packet = grips_given.CommandAcknowledgement.from_buffer_copy(
+            self.stream.recv(32768)
+        )
+        self.queue.put(DecodedCommandAck(packet))
