2a3c5c8fb58f0d8eceff4b300103f9dc30b38203
Reed Masek <masek014@umn.edu>
2026-01-12 20:30:14 +0000

    Added tests for kernel control and PPS

---
+++ impisc/i2c/tests/test_ds3231.py
@@ -0,0 +1,43 @@
+from impisc.i2c.devices.ds3231 import DS3231
+
+
+def test_kernel_control():
+    """Test the kernel control conditions."""
+    device = DS3231(1, 0x68)
+    assert (device.kernel_control), "Kernel should have control by default"
+    device.release_from_kernel()
+    assert not (device.kernel_control)
+    device.release_from_kernel()
+    assert not (device.kernel_control)
+    device.give_to_kernel()
+    assert (device.kernel_control)
+
+
+def test_pps():
+    """Test toggling the PPS; ensures enabling/disabling/toggling."""
+    device = DS3231(1, 0x68)
+    device.release_from_kernel()
+
+    starting_state = device.pps_enabled
+    device.toggle_pps()
+    assert starting_state != device.pps_enabled, "PPS toggle failed"
+    device.toggle_pps()
+    assert starting_state == device.pps_enabled
+
+    device.enable_pps()
+    assert device.pps_enabled == True
+
+    device.disable_pps()
+    assert device.pps_enabled == False
+
+    device.enable_pps()
+    device.enable_pps()
+    assert device.pps_enabled == True
+    
+    device.disable_pps()
+    device.disable_pps()
+    assert device.pps_enabled == False
+
+
+if __name__ == "__main__":
+    test_pps()
