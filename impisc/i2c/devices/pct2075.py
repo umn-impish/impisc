2e84eae6819d6bcfdb66260d16cfc40f2615bf6d
Reed Masek <masek014@umn.edu>
2026-01-12 22:22:03 +0000

    Added idle time configuration with test

--- impisc/i2c/devices/pct2075.py
+++ impisc/i2c/devices/pct2075.py
@@ -14,12 +14,13 @@ class PCT2075(GenericDevice):
         super().__init__(bus_number=bus_number, address=address)
         self.add_register(Register("temp", 0x00, 16))
         self.add_register(Register("conf", 0x01, 8))
+        self.add_register(Register("tidle", 0x04, 8))
 
     @property
     def conf_register(self) -> int:
         """The current value in the conf register (0x01)."""
         return self.read_block_data("conf")
-    
+
     @property
     def is_shutdown(self) -> bool:
         """Indicates whether the device is shutdown (True) or not (False)."""
@@ -28,10 +29,20 @@ class PCT2075(GenericDevice):
     def shutdown(self):
         """Set the device to shutdown mode."""
         self.write_block_data("conf", self.conf_register | 0b00000001)
-    
+
     def wakeup(self):
         """Wakeup the device from shutdown mode."""
         self.write_block_data("conf", self.conf_register & 0b11111110)
+
+    def set_idle_time(self, value: float) -> float:
+        """Sets the value in the tidle register (0x04).
+        Valid values range from [0.1, 3.1] seconds, at 0.1 s increments.
+        value is rounded to the nearest valid value; the set value is
+        returned.
+        """
+        if (value < 0.1) or (value > 3.1):
+            raise ValueError(f"Given idle time must be within [0.1, 3.1]; not {value}")
+        self.write_block_data("tidle", int(round(value, 1) * 10))
 
     def read_temperature(self) -> float:
         """Read the temperature from the temp register (0x00),
--- impisc/i2c/tests/test_pct2075.py
+++ impisc/i2c/tests/test_pct2075.py
@@ -38,5 +38,22 @@ def test_temperature_reading():
     device.read_temperature()
 
 
-if __name__ == '__main__':
-    test_shutdown()
+def test_set_tidle():
+    """Test setting the idle time between temperature measurements."""
+    device = PCT2075(1, 0x49)
+    device.wakeup()
+    device.set_idle_time(0.2)
+    device.set_idle_time(0.16)
+    device.set_idle_time(1.4989561561)
+    device.set_idle_time(3.1)
+    try:
+        device.set_idle_time(0.01)
+    except ValueError as e:
+        print("failed to set to 0.01: good")
+        print(e)
+    try:
+        device.set_idle_time(3.11)
+    except ValueError as e:
+        print("failed to set to 3.11: good")
+        print(e)
+    device.set_idle_time(0.1)
