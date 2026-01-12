2ef3d4d40dca27835698ac1666029205928944a4
Reed Masek <masek014@umn.edu>
2026-01-12 21:56:56 +0000

    Add ability to shutdown/wakeup the device

--- impisc/i2c/devices/pct2075.py
+++ impisc/i2c/devices/pct2075.py
@@ -7,7 +7,7 @@ from .device import GenericDevice, Register, int_to_twos_complement
 
 class PCT2075(GenericDevice):
     """Interface with a connected PCT2075 temperature sensor.
-    TODO: add and configure hysteresis register.
+    TODO: add and configure hysteresis register?
     """
 
     def __init__(self, bus_number: int, address: int):
@@ -15,8 +15,26 @@ class PCT2075(GenericDevice):
         self.add_register(Register("temp", 0x00, 16))
         self.add_register(Register("conf", 0x01, 8))
 
+    @property
+    def conf_register(self) -> int:
+        """The current value in the conf register (0x01)."""
+        return self.read_block_data("conf")
+    
+    @property
+    def is_shutdown(self) -> bool:
+        """Indicates whether the device is shutdown (True) or not (False)."""
+        return bool(self.conf_register & 1)
+
+    def shutdown(self):
+        """Set the device to shutdown mode."""
+        self.write_block_data("conf", self.conf_register | 0b00000001)
+    
+    def wakeup(self):
+        """Wakeup the device from shutdown mode."""
+        self.write_block_data("conf", self.conf_register & 0b11111110)
+
     def read_temperature(self) -> float:
-        """Read the temperature from the 0x00 register (temp),
+        """Read the temperature from the temp register (0x00),
         returned in degrees Celsius.
         """
         comp = (
