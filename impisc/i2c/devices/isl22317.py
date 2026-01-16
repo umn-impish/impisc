0909c48963ff35e228c5e5cdd294f2ab8fe28804
Reed B. Masek <masek014@umn.edu>
2026-01-16 16:49:05 -0600

    ISL22317 additions (#19)
    * Reworked wiper reading/writing, add functionality for other device modes
    
    * Add tests for ISL22317
    
    * Ran ruff on set_bias.py
    
    * Set other device modes in set_bias.py
    
    * Fixed typo
    
    * make set_bias executable w usr bin env
    
    * set_bias added to deployment
    
    * ruff on isl file
    
    * ruff on isl test
    
    * Added Literal typing for mode property
    
    * Use pytest.raises in tests
    
    ---------
    
    Co-authored-by: Willy <willy@me.com>

--- impisc/i2c/devices/isl22317.py
+++ impisc/i2c/devices/isl22317.py
@@ -2,6 +2,10 @@
 Defines a class for interfacing with a ISL22317 I2C digital potentiometer.
 """
 
+import time
+
+from typing import Literal
+
 from .device import GenericDevice, Register
 
 
@@ -14,15 +18,96 @@ class ISL22317(GenericDevice):
         self.add_register(Register("mode", 0x01, 8))  # MSR
         self.add_register(Register("control", 0x02, 8))  # ACR
 
-    def read_wiper(self) -> int:
-        """Returns the value stored in the wiper register (0x00)."""
-        value = self.read_block_data("wiper")
-        return value
+    @property
+    def wiper(self) -> int:
+        """The value stored in the wiper register (0x00).
+        Valid values are integers within [0, 127].
+        """
+        return self.read_block_data("wiper")
 
-    def write_wiper(self, value: int):
+    @wiper.setter
+    def wiper(self, value: int):
         """Writes the provided value to the wiper register.
-        value must be within [0, 127], otherwise a ValueError is raised.
+        value must be an integer within [0, 127].
+        Values outside this range raises a ValueError.
         """
         if value not in range(0, 128):
-            raise ValueError("Wiper outside valid range: [0, 127]")
+            raise ValueError(
+                f"Provided wiper value ({value}) outside valid  range: [0, 127]"
+            )
+        while self.writing:
+            time.sleep(0.005)
         self.write_block_data("wiper", value)
+        time.sleep(0.05)
+        # Trying to interface (read/write) with the device immediately
+        # after writing causes a remote I/O error, so we sleep.
+
+    @property
+    def mode_register(self) -> int:
+        """The current value of the mode register (MSR; 0x01)."""
+        return self.read_block_data("mode")
+
+    @property
+    def control_register(self) -> int:
+        """The current value of the control register (ACR; 0x02)."""
+        return self.read_block_data("control")
+
+    @property
+    def awake(self) -> bool:
+        """Specfies whether the device is awake (True) or shutdown (False)
+        by reading the SHDN bit in the control register.
+        """
+        return bool((self.read_block_data("control") >> 6) & 1)
+
+    @awake.setter
+    def awake(self, state: bool):
+        """Set the device to be awake (True) or shutdown (False)."""
+        if state:
+            self.write_block_data("control", self.control_register | 0b01000000)
+        else:
+            self.write_block_data("control", self.control_register & 0b10111111)
+
+    @property
+    def writing(self) -> bool:
+        """Returns the value of the WIP bit in the access control register (0x02).
+        True means a non-volatile write operation is in progress, meaning
+        the wiper and control registers cannot be written to.
+        """
+        return bool((self.control_register >> 5) & 1)
+
+    @property
+    def mode(self) -> Literal["two-terminal", "three-terminal"]:
+        """The device mode, either two-terminal (rheostat) or
+        three-terminal (voltage divier).
+        """
+        bit = (self.mode_register >> 7) & 1
+        return "three-terminal" if bit else "two-terminal"
+
+    @mode.setter
+    def mode(self, new_mode: Literal["two-terminal", "three-terminal"]):
+        """Set the mode of the device; either "two-terminal" or "three-terminal"."""
+        match new_mode:
+            case "two-terminal":
+                self.write_block_data("mode", self.mode_register & 0b01111111)
+            case "three-terminal":
+                self.write_block_data("mode", self.mode_register | 0b10000000)
+            case _:
+                raise ValueError(  # pyright: ignore[reportUnreachable]
+                    f"Provided mode ({new_mode}) is invalid; "
+                    + 'must either be "two-terminal" or "three-terminal"'
+                )
+        time.sleep(0.05)
+
+    @property
+    def precision_mode(self) -> bool:
+        """Specifies whether precision mode is being used."""
+        return not ((self.mode_register >> 6) & 1)
+
+    @precision_mode.setter
+    def precision_mode(self, state: bool):
+        """Sets the state of precision mode; on (True) or off (False)."""
+        if state:
+            self.write_block_data("mode", self.mode_register & 0b10111111)
+        else:
+            self.write_block_data("mode", self.mode_register | 0b01000000)
+        time.sleep(0.05)
