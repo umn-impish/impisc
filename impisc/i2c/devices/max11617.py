a1f0d54101ff9f80d328776582cd613aad195b6d
Reed Masek <masek014@umn.edu>
2026-01-16 21:39:09 +0000

    Better typing; use struct to unpack conversion

--- impisc/i2c/devices/max11617.py
+++ impisc/i2c/devices/max11617.py
@@ -3,6 +3,10 @@ Defines a class allowing primitive control with the MAX11617 analog-to-digital
 converter by Texas Instruments.
 """
 
+import struct
+
+from typing import Literal
+
 import smbus2
 
 from .device import GenericDevice
@@ -13,7 +17,7 @@ class MAX11617(GenericDevice):
 
     # The reference voltage map is stripped down from the fully available
     # functionality since we do not need access to all features.
-    REFERENCE_VOLTAGE_MAP = {
+    REFERENCE_VOLTAGE_MAP: dict[str, int] = {
         "vdd": 0b00000000,
         "external": 0b01000000,  # We will never need this, but included anyway
         "internal": 0b01010000,  # Internal reference, always on
@@ -25,12 +29,12 @@ class MAX11617(GenericDevice):
         # Set attributes to device defaults.
         # We track these as attributes since the register
         # values cannot be read from the device.
-        self._reference = "vdd"
-        self.external_clock = False
-        self.bipolar = False
-        self._scan = 0
-        self._channel = 0
-        self.single_ended = True
+        self._reference: Literal["vdd", "external", "internal"] = "vdd"
+        self.external_clock: bool = False
+        self.bipolar: bool = False
+        self._scan: Literal[0, 1, 2, 3] = 0
+        self._channel: Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] = 0
+        self.single_ended: bool = True
         self.reset_config_register()
 
     @property
@@ -50,12 +54,12 @@ class MAX11617(GenericDevice):
         return (self.scan << 5) + (self.channel << 1) + self.single_ended
 
     @property
-    def channel(self) -> int:
+    def channel(self) -> Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]:
         """The currently selected input channel."""
         return self._channel
 
     @channel.setter
-    def channel(self, new_channel: int):
+    def channel(self, new_channel: Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]):
         """Set the desired input channel.
         Must be within [0, 11].
         """
@@ -67,13 +71,13 @@ class MAX11617(GenericDevice):
         self._write_config_register()
 
     @property
-    def scan(self) -> int:
+    def scan(self) -> Literal[0, 1, 2, 3]:
         """The current value for the two scan bits."""
         return self._scan
 
     @scan.setter
-    def scan(self, new_scan: int):
-        """Set the value of the scan bits: 0, 1, 2, or 3."""
+    def scan(self, new_scan: Literal[0, 1, 2, 3]):
+        """Set the value of the scan bits."""
         if new_scan not in range(0, 4):
             raise ValueError(
                 f"scan bits must be within [0, 3], value {new_scan} is invalid"
@@ -82,23 +86,22 @@ class MAX11617(GenericDevice):
         self._write_config_register()
 
     @property
-    def reference(self) -> str:
+    def reference(self) -> Literal["vdd", "external", "internal"]:
         """The source of the reference voltage."""
         return self._reference
 
     @reference.setter
-    def reference(self, new_reference: str):
+    def reference(self, new_reference: Literal["vdd", "external", "internal"]):
         """Set the source of the reference voltage:
         "vdd", "internal", or "external".
         """
-        new_reference = new_reference.lower()
         if new_reference in self.REFERENCE_VOLTAGE_MAP.keys():
             self._reference = new_reference
             self._write_setup_register()
         else:
             raise ValueError(
                 f'Desired reference "{new_reference}" invalid;'
-                f"must be one of: {self.REFERENCE_VOLTAGE_MAP.keys()}"
+                + f"must be one of: {self.REFERENCE_VOLTAGE_MAP.keys()}"
             )
 
     def _write_setup_register(self):
@@ -107,35 +110,37 @@ class MAX11617(GenericDevice):
         """
         self.bus.write_byte(self.address, self.setup_register)
 
-    def _write_config_register(self) -> int:
-        """Write the configuration register to the device based
-        on set parameters. Returns the register value.
-        """
+    def _write_config_register(self):
+        """Write the configuration register to the device based on set parameters."""
         return self.bus.write_byte(self.address, self.config_register)
 
     def reset_config_register(self):
         """Resets the configuration register to its default."""
         self.bus.write_byte(self.address, 0b10000000)
 
-    def read_conversion(self, which: int, vref: float | None = None) -> float:
+    def read_conversion(
+        self,
+        which: Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        vref: float | None = None,
+    ) -> float:
         """Read the voltage from the specified channel.
-        which must be between [0, 11]. The reference voltage "vref"
-        must be specified if not using the device's internal reference.
+        which specifies the channel between [0, 11]. The reference voltage "vref"
+        must be provided if not using the device's internal reference.
         """
         if which not in range(0, 12):
             raise ValueError(f"Selected channel must be within [0, 11], not {which}")
-        self.channel = which
-        read = smbus2.i2c_msg.read(self.address, 2)
-        self.bus.i2c_rdwr(read)
-        msb, lsb = list(read)
-        value = ((msb & 0x0F) << 8) + lsb
         if self.reference == "internal":
             vref = 2.048
         elif vref is None:
             raise ValueError(
                 f"Reference voltage is configured to {self.reference}, "
-                "but vref was not specified. Please specify the "
-                "vref argument."
+                + "but vref was not specified. Please specify the "
+                + "vref argument."
             )
+        self.channel = which
+        read = smbus2.i2c_msg.read(self.address, 2)
+        self.bus.i2c_rdwr(read)
+        value: int = struct.unpack(">h", bytes(read))[0]  # pyright: ignore[reportAny]
+        value &= 0xFFF  # Flip first four bits to zero since they're always high
 
         return value / (2**12) * vref
