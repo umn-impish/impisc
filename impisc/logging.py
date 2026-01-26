fa0a1d626d98f29135ddc5b2b429537fcbfde148
William Setterberg <william.s.setterberg@gmail.com>
2026-01-25 19:11:51 -0600

    health packet structure/program attempt

---
+++ impisc/logging.py
@@ -0,0 +1,15 @@
+"""
+Should log to systemd. How do we do that??
+"""
+
+def log_debug(s: str):
+    print("debug", s)
+
+def log_info(s: str):
+    print("info", s)
+
+def log_warning(s: str):
+    print("warning", s)
+
+def log_error(s: str):
+    print("error", s)
