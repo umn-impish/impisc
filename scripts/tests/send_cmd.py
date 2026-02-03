2f8e6d803cf85afd265b3ddcce2c825af0dc3ee0
Dorcas Oseni <dorcasdupe16@gmail.com>
2026-02-03 18:07:01 +0000

    for debug or lab purpose

--- scripts/tests/send_cmd.py
+++ scripts/tests/send_cmd.py
@@ -1,14 +1,14 @@
 import json
 import socket
 
-# also random IPs and ports for now
+# for debug
 
 fc_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 command = {
     "version": 1,
     "cmd": "set_mode",
-    "mode": "science",
+    "mode": "debug",
     "params": {
         "n_waveforms": 3000,
         "n_spectra": 320
@@ -16,5 +16,8 @@ command = {
 }
 
 msg_bytes = json.dumps(command).encode('utf-8')
-receiver = ("127.0.0.1", 5000)
+receiver = ("192.168.0.2", 8080)
 fc_sock.sendto(msg_bytes, receiver)
+
+# fc_sock.settimeout(2.0)
+# ack, _ = fc_sock.recfrom(1024)
