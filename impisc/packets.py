09ffa267ccc818db528a482e84318670f4771d1b
Reed Masek <masek014@umn.edu>
2026-02-10 17:56:52 +0000

    Add swap measurement

--- impisc/packets.py
+++ impisc/packets.py
@@ -44,6 +44,7 @@ class HealthPacket(ctypes.LittleEndianStructure):
         ("cpu2_usage", ctypes.c_uint16),
         ("cpu3_usage", ctypes.c_uint16),
         ("ram_usage", ctypes.c_uint16),
+        ("swap_usage", ctypes.c_uint16),
         ("unix_timestamp", ctypes.c_uint32),
         # Padding on the end of the health packet:
         # remove bytes from this as needed
