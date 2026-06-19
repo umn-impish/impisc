59508107b305b2d75e1a542a65d3aa9cc8278f18
Grimpish Elitedesk <grimpish@elitedesk>
2026-06-19 17:30:56 -0500

    GS use tun1 instead of tun0

--- ground_station/deployment/tun0-trigger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Configure the addresses for the SSH VPN on tun0
-# This script should be automatically executed by
-# networkd-dispatcher if tun0 ever goes down.
-
-if [ "$IFACE" = "tun0" ]; then
-    ip addr add 10.0.0.1 peer 10.0.0.2/30 dev tun0
-    ip link set tun0 up
-fi
